d6d29c2caf38e028bb9185cce914a011e959b672
CelestialBlink <kkrakenbreaker@gmail.com>
2020-10-24 19:00:31 -0500

    conexion creada

--- conection/mongoConnection.js
+++ conection/mongoConnection.js
@@ -1,7 +1,7 @@
 const {connect}=require('mongoose');
 
 // let url="mongodb://127.0.0.1:27017/sharelinks";
-let urlAtlas="mongodb+srv://"+process.env.USERDB+":"+process.env.PASSDB+"@cluster0.gt8zx.mongodb.net/<dbname>?retryWrites=true&w=majority";
+let urlAtlas="mongodb+srv://"+process.env.USERDB+":"+process.env.PASSDB+"@cluster0.gt8zx.mongodb.net/sharelinks?retryWrites=true&w=majority";
 
 const getConection=async()=>{
     let connection=await connect(urlAtlas,{
